d9dffa6d8e4adc015d73edc6ce8612f59f73aac8
Alan Torres <62818545+alantorres4@users.noreply.github.com>
2020-09-03 11:54:43 -0500

    Update main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import pyaudio
+import numpy as np
+
+CHUNK = 4096 # number of data points to read at a time
+RATE = 44100 # time resolution of the recording devize (Hz)
+
+p = pyaudio.PyAudio() # initialize the p object
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=RATE, input=True, frames_per_buffer=CHUNK) # uses default input device
+
+# create a numpy array holding a single read of audio data:
+for i in range(10): # do it a few times just to see
+    data = np.fromstring(stream.read(CHUNK), dtype=np.int16)
+    print(data)
+
+# close the stream 
+stream.stop_stream()
+stream.close()
+p.terminate
